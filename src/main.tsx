3af9d311db365087af40a464c0cefcd266c11f84
Miha77777 <lv11miha@gmail.com>
2024-06-21 15:34:54 +0300

    change main

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "./reset.scss";
-import "./main.scss";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
